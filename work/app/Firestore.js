6d5963eed68c783fcf6d26130b6ceb19d5b7a335
Velda Kiara <veldakarimi@gmail.com>
2020-05-03 12:35:06 +0300

    completed lab and deployed to Firebase

--- work/app/Firestore.js
+++ work/app/Firestore.js
@@ -1,1 +1,21 @@
-
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyBrkI2jtF91Vi-fYIAp0djUhNFaABriHbA',
+    authDomain: 'cloud-firestore-a8dd6.firebaseapp.com',
+    projectId: 'cloud-firestore-a8dd6',
+  });
+  
+  const db = firebase.firestore();
+  // enable offline persistence
+db.enablePersistence().catch(err => {
+    if (err.code == "failed-precondition") {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == "unimplemented") {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
+  
